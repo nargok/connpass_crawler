b8d00754bf7413f5480b97e82c783a52c0790fdb
nargok <nargok@gmail.com>
2020-12-11 14:16:38 +0900

    chagne timestamp type

--- save_csv.py
+++ save_csv.py
@@ -14,8 +14,20 @@ def read_json_files(events):
     return events
 
 
+def change_timestamp_type(df):
+    # 末尾が_atのカラムをdatetime型に変換する
+    datetime_columns = filter(lambda a: a[-3:] == '_at', df.columns)
+    for column in datetime_columns:
+        df[column] = pandas.to_datetime(df[column], unit='s')
+
+    return df
+
+
 def save_csv(events):
     df = pandas.json_normalize(events)
+
+    # datetimeの変換 Unix Timestamp型で日時がくるものについてはこの変換処理をする
+    # df = change_timestamp_type(df)
 
     csv_data = df.to_csv()
     csv_data = csv_data.encode("cp932", "ignore")
